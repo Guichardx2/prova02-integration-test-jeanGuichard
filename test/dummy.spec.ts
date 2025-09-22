429494686376f57f8047311c4c4a021bae870a80
Jean Charles Guichard Guichard <jean.276222@alunosatc.edu.br>
2025-09-22 20:43:58 -0300

    test 2

--- test/dummy.spec.ts
+++ test/dummy.spec.ts
@@ -15,20 +15,37 @@ describe('Dummy JSON', () => {
 
   describe('Products', () => {
 
-      it('Get All Products', async () => {
-          await p
-              .spec()
-              .get(`${baseUrl}/products`)
-              .expectStatus(StatusCodes.OK)
-              .expectJsonLike({
-                  products: [
-                      {
-                          id: 1,
-                          title: 'Essence Mascara Lash Princess',
-                      },
-                  ],
-              });
-      });
+    it('Get All Products', async () => {
+      await p
+        .spec()
+        .get(`${baseUrl}/products`)
+        .expectStatus(StatusCodes.OK)
+        .expectJsonLike({
+          products: [
+            {
+              id: 1,
+              title: 'Essence Mascara Lash Princess',
+            },
+          ],
+        });
+    });
+
+    it('Get a Product by ID', async () => {
+      await p
+        .spec()
+        .get(`${baseUrl}/products/26`)
+        .expectStatus(StatusCodes.OK)
+        .expectJsonLike({
+          id: 26,
+          title: "Green Chili Pepper",
+          description: "Spicy green chili pepper, ideal for adding heat to your favorite recipes.",
+          category: "groceries",
+          price: 0.99,
+          discountPercentage: 1,
+          rating: 3.66,
+          stock: 3,
+        });
+    });
 
     it('New Product', async () => {
       await p
@@ -46,6 +63,20 @@ describe('Dummy JSON', () => {
         })
         .expectStatus(StatusCodes.CREATED)
         .returns('id');
+    });
+
+    it ('Update Product', async () => {
+      await p
+        .spec()
+        .patch(`${baseUrl}/products/20`)
+        .withJson({
+          price: 10.99
+        })
+        .expectStatus(StatusCodes.OK)
+        .expectJsonLike({
+          id: 20,
+          price: 10.99
+        });
     });
   });
 });
